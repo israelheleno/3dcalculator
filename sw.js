11094fbe689c2761bb2bcf69589a779195baa8b8
israelheleno <israelheleno@gmail.com>
2026-06-02 12:14:31 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,32 @@
-const CACHE_NAME = 'calc3d-v2';
+const CACHE_NAME = 'calc3d-v3.3';
 const urlsToCache = [
-  '/',
-  'index.html',
-  'style.css',
-  'app.js',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
+  '/3dcalculator/',
+  '/3dcalculator/index.html',
+  '/3dcalculator/style.css',
+  '/3dcalculator/app.js',
+  '/3dcalculator/manifest.json',
+  '/3dcalculator/icon-192.png',
+  '/3dcalculator/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
